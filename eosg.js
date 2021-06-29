9a7d61efe674452c957df48ffae3026aab3fcd22
Rajdeep Roy <rajdeeproy10023@gmail.com>
2021-06-29 21:29:28 +0530

    R

--- eosg.js
+++ eosg.js
@@ -1,3 +1,8 @@
 window.onload=function(){
     var favicon=document.getElementById("favicon");
 }
+
+window.onclick=function(){
+    var button1=document.getElementById("button1");
+    button1=location.href="";
+}
